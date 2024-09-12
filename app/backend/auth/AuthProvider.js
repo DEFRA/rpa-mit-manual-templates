faf435ebab456041f2362321d1fc2f937f266548
shabaz <shabaz.2.ali.external@eviden.com>
2024-09-12 15:17:32 +0100

    add scope

--- app/backend/auth/AuthProvider.js
+++ app/backend/auth/AuthProvider.js
@@ -21,7 +21,7 @@ class AuthProvider {
 
     const authCodeUrlRequestParams = {
       state,
-      scopes: ['process.env.SCOPES'],
+      scopes: [process.env.SCOPES],
       redirectUri: REDIRECT_URI
     }
 
