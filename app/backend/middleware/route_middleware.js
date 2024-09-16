1841dfeb38787cb0fd19166322ba2fd0dc250d69
shabaz <shabaz.2.ali.external@eviden.com>
2024-09-16 23:45:11 +0100

    removed azure allow

--- app/backend/middleware/route_middleware.js
+++ app/backend/middleware/route_middleware.js
@@ -4,20 +4,16 @@ const authPlugin = {
   version: '1.0.0',
   register: async function (server, options) {
     server.ext('onPreHandler', async (request, h) => {
-      if (process.env.EXPRESS_SESSION_SECRET === 'YES') {
-        const publicRoutes = ['/login', '/acquire-token', '/redirect', '/logout']
-        if (publicRoutes.includes(request.path)) {
-          return h.continue
-        }
-        setGlobal('request', request)
-        const isAuthenticated = request.yar.get('isAuthenticated')
-        if (isAuthenticated) {
-          return h.continue
-        } else {
-          return h.redirect('/login').takeover()
-        }
-      } else {
+      const publicRoutes = ['/login', '/acquire-token', '/redirect', '/logout', '/healthz', '/healthy']
+      if (publicRoutes.includes(request.path)) {
         return h.continue
+      }
+      setGlobal('request', request)
+      const isAuthenticated = request.yar.get('isAuthenticated')
+      if (isAuthenticated) {
+        return h.continue
+      } else {
+        return h.redirect('/login').takeover()
       }
     })
   }
