8119dd6043cdcf889063488bb2f67aaac25cb94b
shabaz <shabaz.2.ali.external@eviden.com>
2024-09-18 22:43:34 +0100

    add try catch

--- app/backend/auth/AuthProvider.js
+++ app/backend/auth/AuthProvider.js
@@ -1,6 +1,7 @@
 const msal = require('@azure/msal-node')
 const axios = require('axios')
 const { msalConfig, REDIRECT_URI, POST_LOGOUT_REDIRECT_URI } = require('./authConfig')
+const errorModel = require('../models/commonError')
 
 class AuthProvider {
   constructor (msalConfig) {
@@ -13,51 +14,56 @@ class AuthProvider {
   }
 
   async login (request, h) {
-    const state = this.cryptoProvider.base64Encode(
-      JSON.stringify({
-        successRedirect: '/'
-      })
-    )
+    try {
+      const state = this.cryptoProvider.base64Encode(
+        JSON.stringify({
+          successRedirect: '/'
+        })
+      )
 
-    const authCodeUrlRequestParams = {
-      state,
-      scopes: [process.env.SCOPES],
-      redirectUri: REDIRECT_URI
-    }
-
-    if (!this.msalConfig.auth.cloudDiscoveryMetadata || !this.msalConfig.auth.authorityMetadata) {
-      const [cloudDiscoveryMetadata, authorityMetadata] = await Promise.all([
-        this.getCloudDiscoveryMetadata(this.msalConfig.auth.authority),
-        this.getAuthorityMetadata(this.msalConfig.auth.authority)
-      ])
-
-      this.msalConfig.auth.cloudDiscoveryMetadata = JSON.stringify(cloudDiscoveryMetadata)
-      this.msalConfig.auth.authorityMetadata = JSON.stringify(authorityMetadata)
-    }
-
-    const msalInstance = this.getMsalInstance()
-
-    const { verifier, challenge } = await this.cryptoProvider.generatePkceCodes()
-
-    request.yar.set({
-      pkceCodes: {
-        challengeMethod: 'S256',
-        verifier,
-        challenge
-      },
-      authCodeUrlRequest: {
-        ...authCodeUrlRequestParams,
-        responseMode: msal.ResponseMode.FORM_POST,
-        codeChallenge: challenge,
-        codeChallengeMethod: 'S256'
-      },
-      authCodeRequest: {
-        ...authCodeUrlRequestParams,
-        code: ''
+      const authCodeUrlRequestParams = {
+        state,
+        scopes: [process.env.SCOPES],
+        redirectUri: REDIRECT_URI
       }
-    })
-    const authCodeUrlResponse = await msalInstance.getAuthCodeUrl(request.yar.get('authCodeUrlRequest'))
-    return h.redirect(authCodeUrlResponse)
+
+      if (!this.msalConfig.auth.cloudDiscoveryMetadata || !this.msalConfig.auth.authorityMetadata) {
+        const [cloudDiscoveryMetadata, authorityMetadata] = await Promise.all([
+          this.getCloudDiscoveryMetadata(this.msalConfig.auth.authority),
+          this.getAuthorityMetadata(this.msalConfig.auth.authority)
+        ])
+
+        this.msalConfig.auth.cloudDiscoveryMetadata = JSON.stringify(cloudDiscoveryMetadata)
+        this.msalConfig.auth.authorityMetadata = JSON.stringify(authorityMetadata)
+      }
+
+      const msalInstance = this.getMsalInstance()
+
+      const { verifier, challenge } = await this.cryptoProvider.generatePkceCodes()
+
+      request.yar.set({
+        pkceCodes: {
+          challengeMethod: 'S256',
+          verifier,
+          challenge
+        },
+        authCodeUrlRequest: {
+          ...authCodeUrlRequestParams,
+          responseMode: msal.ResponseMode.FORM_POST,
+          codeChallenge: challenge,
+          codeChallengeMethod: 'S256'
+        },
+        authCodeRequest: {
+          ...authCodeUrlRequestParams,
+          code: ''
+        }
+      })
+      const authCodeUrlResponse = await msalInstance.getAuthCodeUrl(request.yar.get('authCodeUrlRequest'))
+      return h.redirect(authCodeUrlResponse)
+    } catch (error) {
+      console.log(error)
+      return errorModel.errorMessage(error, h)
+    }
   }
 
   async acquireToken (request, h) {
@@ -85,50 +91,61 @@ class AuthProvider {
       if (error instanceof msal.InteractionRequiredAuthError) {
         return this.login(request, h)
       }
-      throw error
+      console.log(error)
+      return errorModel.errorMessage(error, h)
     }
   }
 
   async handleRedirect (request, h) {
-    if (!request.payload || !request.payload.state) {
-      throw new Error('Error: response not found')
+    try {
+      if (!request.payload || !request.payload.state) {
+        throw new Error('Error: response not found')
+      }
+
+      const authCodeRequest = {
+        ...request.yar.get('authCodeRequest'),
+        code: request.payload.code,
+        codeVerifier: request.yar.get('pkceCodes').verifier
+      }
+
+      const msalInstance = this.getMsalInstance()
+
+      if (request.yar.get('tokenCache')) {
+        msalInstance.getTokenCache().deserialize(request.yar.get('tokenCache'))
+      }
+
+      const tokenResponse = await msalInstance.acquireTokenByCode(authCodeRequest)
+
+      request.yar.set({
+        accessToken: tokenResponse.accessToken,
+        tokenCache: msalInstance.getTokenCache().serialize(),
+        idToken: tokenResponse.idToken,
+        account: tokenResponse.account,
+        isAuthenticated: true
+      })
+
+      const state = JSON.parse(this.cryptoProvider.base64Decode(request.payload.state))
+      return h.redirect(state.successRedirect)
+    } catch (error) {
+      console.log(error)
+      return errorModel.errorMessage(error, h)
     }
-
-    const authCodeRequest = {
-      ...request.yar.get('authCodeRequest'),
-      code: request.payload.code,
-      codeVerifier: request.yar.get('pkceCodes').verifier
-    }
-
-    const msalInstance = this.getMsalInstance()
-
-    if (request.yar.get('tokenCache')) {
-      msalInstance.getTokenCache().deserialize(request.yar.get('tokenCache'))
-    }
-
-    const tokenResponse = await msalInstance.acquireTokenByCode(authCodeRequest)
-
-    request.yar.set({
-      accessToken: tokenResponse.accessToken,
-      tokenCache: msalInstance.getTokenCache().serialize(),
-      idToken: tokenResponse.idToken,
-      account: tokenResponse.account,
-      isAuthenticated: true
-    })
-
-    const state = JSON.parse(this.cryptoProvider.base64Decode(request.payload.state))
-    return h.redirect(state.successRedirect)
   }
 
   async logout (request, h) {
-    let logoutUri = `${this.msalConfig.auth.authority}/oauth2/v2.0/`
+    try {
+      let logoutUri = `${this.msalConfig.auth.authority}/oauth2/v2.0/`
 
-    if (POST_LOGOUT_REDIRECT_URI) {
-      logoutUri += `logout?post_logout_redirect_uri=${POST_LOGOUT_REDIRECT_URI}`
+      if (POST_LOGOUT_REDIRECT_URI) {
+        logoutUri += `logout?post_logout_redirect_uri=${POST_LOGOUT_REDIRECT_URI}`
+      }
+
+      request.yar.clear()
+      return h.redirect(logoutUri)
+    } catch (error) {
+      console.log(error)
+      return errorModel.errorMessage(error, h)
     }
-
-    request.yar.clear()
-    return h.redirect(logoutUri)
   }
 
   async getCloudDiscoveryMetadata (authority) {
